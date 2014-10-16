dabd630f72b76e8475b698a6327d3d71a5f27184
Justin Maat <jxm262@gmail.com>
2014-10-16 19:58:59 -0400

    Adding initial page adding font awesome

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,13 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.mvc.{Action, Controller}
+import play.api.http.MimeTypes
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.Index())
   }
 
 }

c1eae328e158cd631e5b44ef104f8fc5fac0e3dc
Justin <jxm262@gmail.com>
2014-10-21 20:34:56 -0400

    Updating Unit tests

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -24,7 +24,9 @@ class ApplicationSpec extends Specification {
 
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+      
+      //spot test the title shows
+      contentAsString(home) must contain ("JMAAT")
     }
   }
 }

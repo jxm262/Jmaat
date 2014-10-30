da11fc827f3219471251d9c991b6729b87f78cab
Justin <jxm262@gmail.com>
2014-10-30 13:24:08 -0400

    -Updating JS to propogate link changes (add/remove) dynamically to the listing on the right side
    -Tested working, will refactor later

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,20 +2,71 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import play.api.mvc.{ Action, Controller }
-import play.api.http.MimeTypes
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+import scala.concurrent.Future
+import reactivemongo.api._
+import play.modules.reactivemongo.MongoController
+import play.modules.reactivemongo.json.collection.JSONCollection
 import models.Posting
-import models.Posting
-import models.Posting
+import reactivemongo.bson.BSONDocument
 
-object Application extends Controller {
+object Application extends Controller with MongoController {
+
+  implicit val postingReads = Json.reads[Posting]
+
+  def collection: JSONCollection = db.collection[JSONCollection]("persons")
 
   def index = Action {
     Redirect("/posting/Home")
   }
-
-  def code(postingId: String) = Action {
-    Ok(views.html.code())
+  
+  def posting(postingId: String) = Action {
+    Ok(views.html.index())
   }
+
+  def admin = Action {
+    Ok(views.html.admin())
+  }  
+
+  def delete = Action.async(parse.json) { implicit request =>
+    Json.fromJson[Posting](request.body).fold(
+      invalid => Future.successful(BadRequest("Bad message form")),
+      valid => collection.remove(Json.obj("postingId" -> valid.postingId)).map(lastError => Ok(Json.obj("success" -> true)))
+    )
+  }
+
+  def save = Action.async(parse.json) { implicit request =>
+    Json.fromJson[Posting](request.body).fold(
+      invalid => Future.successful(BadRequest("Bad message form")),
+      valid => collection.update(Json.obj("postingId" -> valid.postingId), request.body, upsert = true).map(lastError => Ok(Json.obj("success" -> true)))
+    )
+  }
+  
+  def findAllPostings = Action.async {
+    val cursor: Cursor[JsObject] = collection.find(Json.obj()).cursor[JsObject]
+    val futurePersonsList: Future[List[JsObject]] = cursor.collect[List]()
+    val futurePostingsJsonArray: Future[JsArray] = futurePersonsList.map(postings => Json.arr(postings))
+
+    futurePostingsJsonArray.map(postings => Ok(postings))
+  }
+
+  def findPostingById(postingId: String) = Action.async {
+    val cursor: Cursor[JsObject] = collection.find(Json.obj("postingId" -> postingId)).cursor[JsObject]
+    val futurePersonsList: Future[List[JsObject]] = cursor.collect[List]()
+    val futurePersonsJsonArray: Future[JsArray] = futurePersonsList.map(postings => Json.arr(postings))
+    
+    futurePersonsJsonArray.map(postings => Ok(postings))
+  }
+  
+  /**
+   * To map other fields manually, use an explicit read like below, but can use case class instead of tuple
+   */
+  //  val posting: Reads[(String, String, String)] = {
+  //    (JsPath \ "postingId").read[String] and
+  //    (JsPath \ "title").read[String] and
+  //    (JsPath \ "text").read[String]
+  //  }.tupled
 
 }
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,58 +1,4 @@
 /** @jsx React.DOM */
-var PostingBox = React.createClass({
-	loadPostingsFromServer: function(){
-		$.ajax({
-			url: this.state.url,
-			dataType: 'json',
-			success: function(data) {
-				this.setState({data: data[0][0]});
-			}.bind(this),
-			error: function(xhr, status, err) {
-				console.error(this.props.url, status, err.toString());
-			}.bind(this)
-		});
-	},
-	loadLinksFromServer: function(){
-		$.ajax({
-			url: "/api/posting/all",  //this.props.url,
-			dataType: 'json',
-			success: function(data) {
-				this.setState({data: data[0]});
-			}.bind(this),
-			error: function(xhr, status, err) {
-				console.error(this.props.url, status, err.toString());
-			}.bind(this)
-		});
-	},
-	handleLinkClick: function(){
-		return;
-	},
-	getInitialState: function() {
-		return {data: [], url: "/api/posting/" + document.URL.split("/")[4]};
-	},
-	componentDidMount: function() {
-		this.loadPostingsFromServer();
-	},		  
-	render: function() {
-		return (
-			<div className="postingBox">
-				<PostingElem data={this.state.data}/>
-				<div className="col-xs-4 col-md-offset-1 col-md-4">
-					<h1></h1>
-					<div className="panel panel-primary">
-						<div className="panel-heading">
-							<h3 className="panel-title">Code Snippets and Such</h3>
-						</div>
-						<div className="panel-body">
-							<PostingList linkClickHandler={this.handleLinkClick}/>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
-});
-
 var AdminBox = React.createClass({
 	loadPostingsFromServer: function(){
 		$.ajax({
@@ -84,9 +30,34 @@ var AdminBox = React.createClass({
 	        }.bind(this)
 	      });
 	},
+	handlePostingDelete: function(posting){
+		$("#postingId").val("");
+		$("#title").val("");
+		$("#text").val("");
+
+		var newPostings = this.state.data.filter(function(e){
+			return e.postingId !== posting.postingId;
+		});
+		
+		this.setState({data: newPostings});	
+		
+		$.ajax({
+	    	url: "../api/posting/delete",
+	    	dataType: 'json',
+	    	contentType: 'application/json; charset=utf-8',
+	    	type: 'POST',
+	    	data: JSON.stringify(posting),
+	    	success: function(data) {
+	    		this.setState({data: newPostings});	
+	    	}.bind(this),
+	    	error: function(xhr, status, err) {
+	    		console.error(this.props.url, status, err.toString());
+	    	}.bind(this)
+	    });
+	},
 	handleLinkClick: function(e, postingId){
 		e.preventDefault();
-		
+
 		$.get("../api/posting/" + postingId, function(el){
 			var posting = el[0][0];
 			$("#postingId").val(posting.postingId);
@@ -102,8 +73,8 @@ var AdminBox = React.createClass({
 	},		  
 	render: function() {
 		return (
-			<div className="admingBox">
-				<PostingForm onPostingSubmit={this.handlePostingSubmit}/>
+			<div className="adminBox">
+				<PostingForm onPostingSubmit={this.handlePostingSubmit} onPostingDelete={this.handlePostingDelete}/>
 				<div className="col-xs-4 col-md-offset-1 col-md-4">
 					<h1></h1>
 					<div className="panel panel-primary">
@@ -120,61 +91,20 @@ var AdminBox = React.createClass({
 	}
 });
 
-var HomeBox = React.createClass({
+
+var PostingBox = React.createClass({
 	loadPostingsFromServer: function(){
 		$.ajax({
-			url: "/api/posting/java_design_patterns",  //this.props.url,
+			url: this.state.url,
 			dataType: 'json',
 			success: function(data) {
-				this.setState({data: data[0]});
+				this.setState({posting: data[0][0]});
 			}.bind(this),
 			error: function(xhr, status, err) {
 				console.error(this.props.url, status, err.toString());
 			}.bind(this)
 		});
 	},
-	getInitialState: function() {
-		return {data: []};
-	},
-	componentDidMount: function() {
-		this.loadPostingsFromServer();
-	},		  
-	render: function() {
-		return (
-			<div className="postingBox">
-				<Posting data={this.state.data}/>
-				
-				<div className="col-xs-4 col-md-offset-1 col-md-4">
-					<h1></h1>
-					<div className="panel panel-primary">
-						<div className="panel-heading">
-							<h3 className="panel-title">Code Snippets and Such</h3>
-						</div>
-						<div className="panel-body">
-							<PostingList data={this.state.data} />
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
-});
-
-var PostingElem = React.createClass({
-	render: function() {
-		return (
-			<div className="col-xs-8 col-md-7">
-				<div class="posting">				
-					<h1>{this.props.data.title}</h1>
-					{this.props.data.text}
-					<h4><small><i className="fa fa-calendar"></i> Oct 23, 2014</small></h4>
-				</div>
-	        </div>
-		);
-	}
-});
-
-var PostingList = React.createClass({
 	loadLinksFromServer: function(){
 		$.ajax({
 			url: "/api/posting/all",  //this.props.url,
@@ -186,18 +116,41 @@ var PostingList = React.createClass({
 				console.error(this.props.url, status, err.toString());
 			}.bind(this)
 		});
-	},	
+	},
+	handleLinkClick: function(){
+		return;
+	},
 	getInitialState: function() {
-		return {data: []};
+		return {data: [], posting: [], url: "/api/posting/" + document.URL.split("/")[4]};
 	},
 	componentDidMount: function() {
+		this.loadPostingsFromServer();
 		this.loadLinksFromServer();
-	},		
+	},		  
 	render: function() {
-		
-		var that = this;	//I have a love-hate relationship with js.  This is one reason why i hate :)
+		return (
+			<div className="postingBox">
+				<PostingElem data={this.state.posting}/>
+				<div className="col-xs-4 col-md-offset-1 col-md-4">
+					<h1></h1>
+					<div className="panel panel-primary">
+						<div className="panel-heading">
+							<h3 className="panel-title">Code Snippets and Such</h3>
+						</div>
+						<div className="panel-body">
+							<PostingList data={this.state.data} linkClickHandler={this.handleLinkClick}/>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+});
 
-		var postingNodes = this.state.data.map(function (posting) {
+var PostingList = React.createClass({
+	render: function() {
+		var that = this;	//I have a love-hate relationship with js.  This is one reason why i hate :)
+		var postingNodes = this.props.data.map(function (posting) {
 			return (
 				<ul>
 	        		<PostingLink title={posting.title} link={posting.postingId} linkClickHandler={that.props.linkClickHandler}>
@@ -214,15 +167,16 @@ var PostingList = React.createClass({
 	}
 });
 
-var Posting = React.createClass({
+var PostingElem = React.createClass({
 	render: function() {
 		return (
 			<div className="col-xs-8 col-md-7">
-				<div className="posting">
-					<h1>{this.props.title}</h1>
-					{this.props.text}
+				<div class="posting">				
+					<h1>{this.props.data.title}</h1>
+					{this.props.data.text}
+					<h4><small><i className="fa fa-calendar"></i> Oct 23, 2014</small></h4>
 				</div>
-			</div>
+	        </div>
 		);
 	}
 });
@@ -244,6 +198,23 @@ var PostingLink = React.createClass({
 });
       
 var PostingForm = React.createClass({
+	handleDelete: function(e){
+		e.preventDefault();
+		var postingId = this.refs.postingId.getDOMNode().value.trim();
+		var text = this.refs.text.getDOMNode().value.trim();
+
+		if (!postingId) {
+			return;
+		}
+		
+		this.props.onPostingDelete({"postingId": postingId, "title": postingId, "text": text});		//read up on when to use props vs state		
+		
+		this.refs.postingId.getDOMNode().value = '';
+		this.refs.text.getDOMNode().value = '';
+		this.refs.title.getDOMNode().value = '';
+		
+		return;
+	},
 	handleSubmit: function(e) {
 		e.preventDefault();
 		var postingId = this.refs.postingId.getDOMNode().value.trim();
@@ -287,7 +258,7 @@ var PostingForm = React.createClass({
 						<div className="col-sm-offset-2 col-sm-10">
 							<button type="submit" className="btn btn-primary" id="submit" value="Post">Submit</button>
 							&nbsp;&nbsp;&nbsp;
-							<button type="submit" className="btn btn-danger" id="delete">Delete</button>
+							<button className="btn btn-danger" id="delete" onClick={this.handleDelete}>Delete</button>
 						</div>
 					</div>					
 				</form>
